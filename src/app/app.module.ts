865c50232e10ecad69acf3f88d521de9960e8277
Yupee <gi.velizr@gmail.com>
2020-04-16 16:06:08 -0400

    se crea proyecto con autentificacion usando Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { FormsModule } from '@Angular/forms';
+import { ListComponent } from './list/list.component';
+import { environment } from 'src/environments/environment';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { RegistrationComponent } from './auth/registration/registration.component';
+import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    ListComponent,
+    RegistrationComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFireStorageModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyChIzT462dGHkqToIy4HCsYNiG0h3IMVZQ",
+    authDomain: "testfe-f368f.firebaseapp.com",
+    databaseURL: "https://testfe-f368f.firebaseio.com",
+    projectId: "testfe-f368f",
+    storageBucket: "testfe-f368f.appspot.com",
+    messagingSenderId: "554524358086",
+    appId: "1:554524358086:web:45d8e02aa278c61b032ec7"
+  }
 };
 
 /*
