69c9e8603f627c784517061ab56a529fbdde6f3f
Yupee <gi.velizr@gmail.com>
2020-04-16 23:12:16 -0400

    se agregan las variables necesarias para q en ambiente prod se pueda conectar a firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyChIzT462dGHkqToIy4HCsYNiG0h3IMVZQ",
+    authDomain: "testfe-f368f.firebaseapp.com",
+    databaseURL: "https://testfe-f368f.firebaseio.com",
+    projectId: "testfe-f368f",
+    storageBucket: "testfe-f368f.appspot.com",
+    messagingSenderId: "554524358086",
+    appId: "1:554524358086:web:45d8e02aa278c61b032ec7"
+  }
 };
